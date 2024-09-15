aee3181eac67d62cf2771ba6da64a0e513fca017
Digvijay Narayan <digvijayn1993@gmail.com>
2024-09-16 00:47:28 +0530

    added db connection for google sheets

--- backend/database/db/connection.go
+++ backend/database/db/connection.go
@@ -5,12 +5,21 @@ import (
 	"log"
 	// "os"
 	"sync"
-
-	// "github.com/StackItHQ/pes-ndigvijay/database/models"
+	// "context"
+	"github.com/StackItHQ/pes-ndigvijay/backend/database/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	freedb "github.com/FreeLeh/GoFreeDB"
-	"github.com/FreeLeh/GoFreeDB/google/auth"
+	// freedb "github.com/FreeLeh/GoFreeDB"
+	// "github.com/FreeLeh/GoFreeDB/google/auth"
+	// _ "github.com/lib/pq"
+	// "google.golang.org/api/option"
+	// "google.golang.org/api/sheets/v4"
+)
+
+const (
+	credentialsFile = "/Users/digvijaynarayan/Desktop/superjoin/pes-ndigvijay/backend/servicetoken.json"
+	spreadsheetId    = "1Bxi8B75FyzeLhCfTnk3QXoX4zMeWNdGa4Q_XbRjuQNw"
+	sheetRange       = "Sheet1!A1:D10" 
 )
 
 var DBLock sync.Mutex
@@ -33,32 +42,32 @@ func InitDB() *gorm.DB {
 	return db
 }
 
-var Store *freedb.GoogleSheetRowStore
+// var Store *freedb.GoogleSheetRowStore
 
-func InitSheet() {
-	authenticator, err := auth.NewServiceFromFile(
-		"../servicetoken.json",
-		freedb.FreeDBGoogleAuthScopes,
-		auth.ServiceConfig{},
-	)
-	if err != nil {
-		fmt.Println("Error accessing sheet", err)
-		return
-	}
+// func InitSheet() {
+// 	authenticator, err := auth.NewServiceFromFile(
+// 		"../servicetoken.json",
+// 		freedb.FreeDBGoogleAuthScopes,
+// 		auth.ServiceConfig{},
+// 	)
+// 	if err != nil {
+// 		fmt.Println("Error accessing sheet", err)
+// 		return
+// 	}
 
-	Store = freedb.NewGoogleSheetRowStore(
-		authenticator,
-		"1Bxi8B75FyzeLhCfTnk3QXoX4zMeWNdGa4Q_XbRjuQNw",
-		"Sheet1",
-		freedb.GoogleSheetRowStoreConfig{
-			Columns: []string{
-				"id",        
-				"name",      
-				"email",     
-				"password",  
-				"age",         
-			},
-		},
-	)
+// 	Store = freedb.NewGoogleSheetRowStore(
+// 		authenticator,
+// 		"1Bxi8B75FyzeLhCfTnk3QXoX4zMeWNdGa4Q_XbRjuQNw",
+// 		"Sheet1",
+// 		freedb.GoogleSheetRowStoreConfig{
+// 			Columns: []string{
+// 				"id",        
+// 				"name",      
+// 				"email",     
+// 				"password",  
+// 				"age",         
+// 			},
+// 		},
+// 	)
 	
-}
+// }
